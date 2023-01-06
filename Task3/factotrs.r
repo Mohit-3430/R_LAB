951be87f5a65a69fe8c310a0d58ae53040cd98d0
Mohit Suryadevara <75293052+Mohit-3430@users.noreply.github.com>
2023-01-06 16:41:51 +0530

    Modified to simpler version for Task-3/factors.r

--- Task3/factotrs.r
+++ Task3/factotrs.r
@@ -1,21 +1,13 @@
-n = as.integer(readline(prompt = "Enter a number to find Factors:"))
+n <- as.integer(readline(prompt = "Enter a number to find Factors:"))
 
 
-print_facotrs = function(n)
-{
-  print(paste("The fators of", n, "are:" ))
-  
-  for(i in 1:sqrt(n)){
-    if(n %% i==0){
-      if(n %/% i == i){
-        print(i)
-      }else {
-        print(paste(i, "|", n/i))
-      }
+print_facotrs <- function(n) {
+  print(paste("The fators of", n, "are:"))
+  for (i in 1:n) {
+    if (n %% i == 0) {
+      print(i)
     }
-    
   }
 }
-
 
 print_facotrs(n)
